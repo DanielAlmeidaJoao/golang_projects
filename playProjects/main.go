99ede8f88a7f41ce609b57023cb35635af6705d8
Daniel de Almeida <d.joao@campus.fct.unl.pt>
2024-03-10 22:41:07 +0000

    saving latest features

--- playProjects/main.go
+++ playProjects/main.go
@@ -15,14 +15,10 @@ func testDefer() {
 
 	ola = true
 }
-func main() {
-	//testDefer()
-
+func testBytesConversion() {
 	buf := new(bytes.Buffer)
-	fmt.Println("DEFAULT SIZE: ", buf.Cap(), buf.Available(), buf.AvailableBuffer())
 	var num uint8 = 1
 	err := binary.Write(buf, binary.LittleEndian, num)
-	fmt.Println("DEFAULT SIZE: ", buf.Cap(), buf.Available(), buf.AvailableBuffer())
 
 	if err != nil {
 		fmt.Println("binary.Write failed:", err)
@@ -32,10 +28,57 @@ func main() {
 
 	buf.WriteString("hello")
 	println()
-	fmt.Println(buf.Available(), "olll")
 	fmt.Println(buf.Cap())
 	var dest uint8
 	err = binary.Read(buf, binary.LittleEndian, &dest)
 	println(err)
 	println("PRIGINAL IS:", dest)
+}
+func testAppendConversion() {
+	numbers := make([]int, 10)
+	fmt.Println(numbers)
+	numbers = append(numbers, 123)
+	numbers = append(numbers, 99)
+
+	fmt.Println(numbers)
+
+}
+func testMapLen() {
+	ma := make(map[int]string, 10)
+	fmt.Println(len(ma))
+
+	ma[12] = "OLA"
+	ma[12] = "OLA2"
+	ma[13] = "PPOLA"
+	fmt.Println(ma)
+}
+
+type EventHolder[T any] struct {
+	eventId int
+	data    T
+}
+type Message struct {
+	name   string
+	age    int
+	weight int
+}
+
+func testGenerics() {
+	event := EventHolder[Message]{
+		eventId: 100,
+		data: Message{
+			age:    12,
+			name:   "DANIEL",
+			weight: 123,
+		},
+	}
+
+	fmt.Println(event.eventId, event.data.name, event.data.age, event.data.weight)
+}
+func main() {
+	//testDefer()
+	//testBytesConversion()
+	//testAppendConversion()
+	//testMapLen()
+	testGenerics()
 }
